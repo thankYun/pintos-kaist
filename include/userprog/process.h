3b032e264e696acf1399616bafc37d1149391e3e
Ubuntu <ubuntu@ip-172-31-11-189.ap-northeast-2.compute.internal>
2023-06-05 12:55:32 +0000

    project 2 Argument Passing

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -10,4 +10,6 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (struct thread *next);
 
+//project 2
+void argument_stack(char **parse, int count, void **rsp);
 #endif /* userprog/process.h */
--- userprog/process.c
+++ userprog/process.c
@@ -50,6 +50,10 @@ process_create_initd (const char *file_name) {
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
 
+	//project 2
+	char *save_ptr;
+	strtok_r(file_name,  " ", &save_ptr);
+
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
 	if (tid == TID_ERROR)
@@ -176,8 +180,22 @@ process_exec (void *f_name) {
 	/* We first kill the current context */
 	process_cleanup ();
 
+	//project 2
+	char *parse[64];
+	char *token, *save_ptr;
+	int count = 0;
+	for(token = strtok_r(file_name, " ", &save_ptr); token != NULL; token = strtok_r(NULL, " ", &save_ptr))
+		parse[count++] = token;
+
 	/* And then load the binary */
 	success = load (file_name, &_if);
+
+	//project 2
+	argument_stack(parse, count, &_if.rsp);
+	_if.R.rdi = count;
+	_if.R.rsi = (char*)_if.rsp + 8;
+
+	hex_dump(_if.rsp, _if.rsp, USER_STACK - (uint64_t)_if.rsp, true);
 
 	/* If load failed, quit. */
 	palloc_free_page (file_name);
@@ -189,6 +207,38 @@ process_exec (void *f_name) {
 	NOT_REACHED ();
 }
 
+//project 2
+void argument_stack(char **parse, int count, void **rsp)
+{
+	for(int i = count - 1; i > -1; i--)
+	{
+		for (int j = strlen(parse[i]); j > -1; j--)
+		{
+			(*rsp)--;
+			**(char **)rsp = parse[i][j];
+		}
+		parse[i] = *(char **)rsp;
+	}
+
+	int padding = (int)*rsp % 8;
+	for (int i = 0; i < padding; i++)
+	{
+		(*rsp)--;
+		**(uint8_t **)rsp = 0;
+	}
+
+	(*rsp) -= 8;
+	**(char ***)rsp = 0;
+
+	for (int i = count - 1; i > -1; i--)
+	{
+		(*rsp) -= 8;
+		**(char ***)rsp = parse[i];
+	}
+
+	(*rsp) -= 8;
+	**(void ***)rsp = 0;
+}
 
 /* Waits for thread TID to die and returns its exit status.  If
  * it was terminated by the kernel (i.e. killed due to an
@@ -204,6 +254,13 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+
+	//project 2(임시)
+	for(int i = 0; i < 100000000; i++)
+	{
+
+	}
+
 	return -1;
 }
 
